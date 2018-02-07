1d8f8a160ab1e8152c938c3dd458d633f819990b
Laptop Lars Smid <99039541@mydavinci.nl>
2018-02-07 08:58:31 +0100

    7-2/08:58

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//set default value
 var currentSubject = 0;
 
 //loop door alle  vragen
@@ -6,10 +7,23 @@ for( let subject of subjects){
   //console.log(subject);
 }
 
-function setSubjectNumber(){
-		return currentSubject += 1;
+document.getElementById("title").innerHTML = (subjects[currentSubject].title);
+document.getElementById("statement").innerHTML = (subjects[currentSubject].statement);
+
+function volgende(){
+		currentSubject += 1;
+
+		document.getElementById("title").innerHTML = (subjects[currentSubject].title);
+		document.getElementById("statement").innerHTML = (subjects[currentSubject].statement);
 }
 
+function vorige(){
+		currentSubject -= 1;
+
+		document.getElementById("title").innerHTML = (subjects[currentSubject].title);
+		document.getElementById("statement").innerHTML = (subjects[currentSubject].statement);
+}
+/*
 function checkSubject(){
 	console.log("checkSubject");
 	setSubjectNumber();
@@ -17,8 +31,15 @@ function checkSubject(){
 			console.log("if");
 			document.getElementById("title").innerHTML = (subjects[0].title);
 			document.getElementById("statement").innerHTML = (subjects[0].statement);
-	}else if(currentSubject == 2){
+	}
+
+	else if(currentSubject == 2){
 			document.getElementById("title").innerHTML = (subjects[1].title);
 			document.getElementById("statement").innerHTML = (subjects[1].statement);
+			//gekeuze deel = eens
+	}else if(currentSubject == 3){
+			document.getElementById("title").innerHTML = (subjects[2].title);
+			document.getElementById("statement").innerHTML = (subjects[2].statement);
 	}
 }
+*/
